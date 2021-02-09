d666af6de82c456d597c25083e7af0bc45cd0e54
ilies ouldmenouer <63064471+ilyasxdz@users.noreply.github.com>
2021-02-09 11:00:29 +0100

    react-native-sortable-list setup

--- App.js
+++ App.js
@@ -1,70 +1,225 @@
-import React, {useState, useCallback} from 'react';
-import {View, TouchableOpacity, Text} from 'react-native';
-import DraggableFlatList, {
-  RenderItemParams,
-} from 'react-native-draggable-flatlist';
+/**
+ * Sample React Native App
+ * httpss://github.com/facebook/react-native
+ * @flow
+ */
 
-const NUM_ITEMS = 10;
+import React, {Component} from 'react';
+import {
+  Animated,
+  Easing,
+  StyleSheet,
+  Text,
+  Image,
+  View,
+  Dimensions,
+  Platform,
+} from 'react-native';
+import SortableList from 'react-native-sortable-list';
 
-function getColor(i: number) {
-  const multiplier = 255 / (NUM_ITEMS - 1);
-  const colorVal = i * multiplier;
-  return `rgb(${colorVal}, ${Math.abs(128 - colorVal)}, ${255 - colorVal})`;
-}
+const window = Dimensions.get('window');
 
-const exampleData: Item[] = [...Array(20)].map((d, index) => {
-  const backgroundColor = getColor(index);
-  return {
-    key: `item-${backgroundColor}`,
-    label: String(index),
-    backgroundColor,
-  };
-});
-
-type Item = {
-  key: string,
-  label: string,
-  backgroundColor: string,
+const data = {
+  0: {
+    image: 'https://placekitten.com/200/240',
+    text: 'Chloe',
+  },
+  1: {
+    image: 'https://placekitten.com/200/201',
+    text: 'Jasper',
+  },
+  2: {
+    image: 'https://placekitten.com/200/202',
+    text: 'Pepper',
+  },
+  3: {
+    image: 'https://placekitten.com/200/203',
+    text: 'Oscar',
+  },
+  4: {
+    image: 'https://placekitten.com/200/204',
+    text: 'Dusty',
+  },
+  5: {
+    image: 'https://placekitten.com/200/205',
+    text: 'Spooky',
+  },
+  6: {
+    image: 'https://placekitten.com/200/210',
+    text: 'Kiki',
+  },
+  7: {
+    image: 'https://placekitten.com/200/215',
+    text: 'Smokey',
+  },
+  8: {
+    image: 'https://placekitten.com/200/220',
+    text: 'Gizmo',
+  },
+  9: {
+    image: 'https://placekitten.com/220/239',
+    text: 'Kitty',
+  },
 };
 
-function Example() {
-  const [data, setData] = useState(exampleData);
+export default class Basic extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>React Native Sortable List</Text>
+        <SortableList
+          style={styles.list}
+          contentContainerStyle={styles.contentContainer}
+          data={data}
+          renderRow={this._renderRow}
+        />
+      </View>
+    );
+  }
 
-  const renderItem = useCallback(
-    ({item, index, drag, isActive}: RenderItemParams<Item>) => {
-      return (
-        <TouchableOpacity
-          style={{
-            height: 100,
-            backgroundColor: isActive ? 'red' : item.backgroundColor,
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-          onLongPress={drag}>
-          <Text
-            style={{
-              fontWeight: 'bold',
-              color: 'white',
-              fontSize: 32,
-            }}>
-            {item.label}
-          </Text>
-        </TouchableOpacity>
-      );
-    },
-    [],
-  );
-
-  return (
-    <View style={{flex: 1}}>
-      <DraggableFlatList
-        data={data}
-        renderItem={renderItem}
-        keyExtractor={(item, index) => `draggable-item-${item.key}`}
-        onDragEnd={({data}) => setData(data)}
-      />
-    </View>
-  );
+  _renderRow = ({data, active}) => {
+    return <Row data={data} active={active} />;
+  };
 }
 
-export default Example;
+class Row extends Component {
+  constructor(props) {
+    super(props);
+
+    this._active = new Animated.Value(0);
+
+    this._style = {
+      ...Platform.select({
+        ios: {
+          transform: [
+            {
+              scale: this._active.interpolate({
+                inputRange: [0, 1],
+                outputRange: [1, 1.1],
+              }),
+            },
+          ],
+          shadowRadius: this._active.interpolate({
+            inputRange: [0, 1],
+            outputRange: [2, 10],
+          }),
+        },
+
+        android: {
+          transform: [
+            {
+              scale: this._active.interpolate({
+                inputRange: [0, 1],
+                outputRange: [1, 1.07],
+              }),
+            },
+          ],
+          elevation: this._active.interpolate({
+            inputRange: [0, 1],
+            outputRange: [2, 6],
+          }),
+        },
+      }),
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.active !== nextProps.active) {
+      Animated.timing(this._active, {
+        duration: 300,
+        easing: Easing.bounce,
+        toValue: Number(nextProps.active),
+      }).start();
+    }
+  }
+
+  render() {
+    const {data, active} = this.props;
+
+    return (
+      <Animated.View style={[styles.row, this._style]}>
+        <Image source={{uri: data.image}} style={styles.image} />
+        <Text style={styles.text}>{data.text}</Text>
+      </Animated.View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#eee',
+
+    ...Platform.select({
+      ios: {
+        paddingTop: 20,
+      },
+    }),
+  },
+
+  title: {
+    fontSize: 20,
+    paddingVertical: 20,
+    color: '#999999',
+  },
+
+  list: {
+    flex: 1,
+  },
+
+  contentContainer: {
+    width: window.width,
+
+    ...Platform.select({
+      ios: {
+        paddingHorizontal: 30,
+      },
+
+      android: {
+        paddingHorizontal: 0,
+      },
+    }),
+  },
+
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    padding: 16,
+    height: 80,
+    flex: 1,
+    marginTop: 7,
+    marginBottom: 12,
+    borderRadius: 4,
+
+    ...Platform.select({
+      ios: {
+        width: window.width - 30 * 2,
+        shadowColor: 'rgba(0,0,0,0.2)',
+        shadowOpacity: 1,
+        shadowOffset: {height: 2, width: 2},
+        shadowRadius: 2,
+      },
+
+      android: {
+        width: window.width - 30 * 2,
+        elevation: 0,
+        marginHorizontal: 30,
+      },
+    }),
+  },
+
+  image: {
+    width: 50,
+    height: 50,
+    marginRight: 30,
+    borderRadius: 25,
+  },
+
+  text: {
+    fontSize: 24,
+    color: '#222222',
+  },
+});
